a3ef9b3bbc75a1bdb6a4f2cccb6be6fc6249c67e
Facundo Rojas <facuroja@hotmail.com>
2022-10-04 19:20:56 -0300

    LAB2 JS: Clase 3 Tipos de Datos

---
+++ leccion02/02-01-tiposDatos.js
@@ -0,0 +1,14 @@
+// Tipos de datos en JavaScript
+
+var nombre = 'Facundo';//Tipo Str
+console.log(nombre);
+
+var numero = 3000; //Tipo Int
+console.log(numero);
+
+var objeto ={ //Tipo Object
+    nombre : 'Facundo',
+    apellido : 'Rojas',
+    telefono : '123321321'
+}
+console.log(objeto);
