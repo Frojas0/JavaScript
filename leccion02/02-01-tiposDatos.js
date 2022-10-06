d32e4c271ba4b500bdb3636dfa42158dd7c815df
Facundo Rojas <facuroja@hotmail.com>
2022-10-06 19:36:14 -0300

    LAB2 JS: Clase 4 Tipos de Datos Parte 2
    Continuamos viendo tipos de datos en JS y su visualización en la consola del navegador

--- leccion02/02-01-tiposDatos.js
+++ leccion02/02-01-tiposDatos.js
@@ -1,14 +1,39 @@
 // Tipos de datos en JavaScript
 
-var nombre = 'Facundo';//Tipo Str
+//Tipo String
+var nombre = 'Facundo';
 console.log(nombre);
 
-var numero = 3000; //Tipo Int
+//Tipo Number
+var numero = 3000;
 console.log(numero);
 
-var objeto ={ //Tipo Object
+//Tipo Object
+var objeto ={
     nombre : 'Facundo',
     apellido : 'Rojas',
     telefono : '123321321'
 }
 console.log(objeto);
+
+//Tipo Boolean
+var bandera = true;
+console.log(bandera);
+
+//Tipo Function
+//funcion nos permite reutilizar lineas de codigo
+function miFuncion(){}
+console.log(miFuncion);
+
+//Tipo Symbol
+var simbolo = Symbol("Mi simbolo");
+console.log(simbolo);
+
+//Tipo Class
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(Persona);
