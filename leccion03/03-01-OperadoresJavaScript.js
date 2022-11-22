c9ae570673a3aaadadda2cacd5b32d88f51c9ba7
Facundo Rojas <facuroja@hotmail.com>
2022-11-22 18:18:03 -0300

    LAB2 JS: Clase 8 y 9
    Clase-8/9-js; Vemos diferencias entre var, let y const, algunos operadores y funciones como isNaN

--- leccion03/03-01-OperadoresJavaScript.js
+++ leccion03/03-01-OperadoresJavaScript.js
@@ -13,3 +13,48 @@ if(edad >= mayor){
 } else{
     console.log("Es menor de edad")
 }
+
+//Ejercicio: Dentro de un rango
+let valor = 9, valMin = 0, valMax = 10;
+if(valor >= valMin && valor <= valMax){
+    console.log("El valor esta dentro del rango")
+} else{
+    console.log("El valor esta fuera de rango")
+}
+
+//Ejercicio: El padre puede o no asistir al juego de su hijo
+let vacaciones = true, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+} else{
+    console.log("El padre no puede asistir al juego del hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "verdadero" : "falso"
+console.log(resultado2)
+
+let numero2 = 9;
+resultado2 = numero2 % 2 == 0 ? "es un numero par" : "es un numero impar";
+console.log(resultado2)
+
+//Convertir String a Number
+let miNumero = "16";
+console.log(typeof miNumero);
+let edad2 = Number(miNumero);
+console.log(edad2);
+
+//Funcion isNaN
+if(isNaN(edad2)){ //is Not a Number (devuelve un resultado booleano)
+    console.log("esta variable no contiene solo numeros")
+}else{
+    if(edad2 >= 16){
+        console.log("Puede votar");
+    } else{
+        console.log("No puede votar");
+    }
+}
+
+
+let resultado3 = edad2 >= 18 ? "Puede votar" : "No puede votar";
+ console.log(resultado3);
