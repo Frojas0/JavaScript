494a5819058e98b353ca459c0738dee21925032e
Facundo Rojas <facuroja@hotmail.com>
2022-11-22 16:45:41 -0300

    LAB2 JS: Clase 7 Similitudes con Java
    Clase-7-js, Revisamos similitudes y realizamos ejercicios

--- leccion02/02-01-ConcatenacionDeCadenas.js
+++ leccion02/02-01-ConcatenacionDeCadenas.js
@@ -17,3 +17,25 @@ console.log(juntos);
 nombre += apellido; // Concatenacion utilizando operador simplificado
 console.log(nombre);
 nombre = 'Jose';
+
+// Hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lopez";
+//apellido2 = "peres"; una constante no puede ser modificada
+console.log(apellido2)
+
+let x,y; // Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; // Se puede hacer asignacion de varias variables dentro de la misma linea
+let z = x + y; // Se asigna el valor de la operacion
+console.log(z);
+
+//let 1num = 31; Las variables no pueden comenzar con un numero
+//let break = "abc"; Tampoco pueden ser nombradas con palabras reservadas
+
+let _1num = 31;
+let rompiendo = "rompe"
+
+console.log(_1num);
+console.log(rompiendo);
---
+++ leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,15 @@
+//Ejercicio: para encontrar numeros pares e impares
+let numero = 12;
+if(numero %2 == 0){
+    console.log("Es un numero Par")
+} else{
+    console.log("Es un numero impar")
+}
+
+//Ejercicio: es mayor de edad
+let edad = 16, mayor = 18;
+if(edad >= mayor){
+    console.log("Es mayor de edad")
+} else{
+    console.log("Es menor de edad")
+}
